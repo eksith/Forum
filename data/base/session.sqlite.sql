db722abd1ed20b46bbfb4692df3e7ac9e14463da
Pep҉p̡er G̸en҉e̸tik <eksith@live.com>
2016-12-21 11:26:08 -0500

    Create session.sqlite.sql

---
+++ data/base/session.sqlite.sql
@@ -0,0 +1,23 @@
+CREATE TABLE sessions (
+	id TEXT PRIMARY KEY NOT NULL,
+	skey TEXT DEFAULT '',			-- Composite key
+	sdata TEXT DEFAULT NULL,		-- Encrypted session data
+	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_sessions_on_updated_at ON sessions ( updated_at DESC );
+
+CREATE TRIGGER session_after_update AFTER UPDATE ON sessions FOR EACH ROW 
+WHEN NEW.updated_at < OLD.updated_at
+BEGIN
+	UPDATE sessions SET updated_at = CURRENT_TIMESTAMP 
+		WHERE rowid = NEW.rowid;
+END;
+
+-- Expire session after 1 hour of inactivity
+CREATE TRIGGER session_gc AFTER UPDATE ON sessions FOR EACH ROW
+WHEN ( ABS( RANDOM() ) % 49 + 1 ) % 5 = 0
+BEGIN
+	DELETE FROM sessions 
+	WHERE strftime('%s', updated_at) < ( strftime('%s','now') - 3600 );
+END;
